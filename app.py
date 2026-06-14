f83081f77177e3ed5e791b93150fb2555d4c0365
Shukdev Datta <99469701+shukdevtroy@users.noreply.github.com>
2026-06-14 20:40:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1263,5 +1263,6 @@ def build_ui():
 if __name__ == "__main__":
     demo = build_ui()
     demo.launch(
-        share=True
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
     )
